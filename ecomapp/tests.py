e0225951a0804d554b351d0b9d03e7adf83b2a09
minh10022001 <minh0353845565@gmail.com>
2023-05-03 01:00:42 +0700

    hi

--- ecomapp/tests.py
+++ ecomapp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.contrib.auth.models import User
+from .models import *
+
+print(Product.objects.all)
--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-from google_trans_new import google_translator  
-translator = google_translator()  
-text= 'mặt hàng này rất tệ, không đúng với giá thành'
-translate_text = translator.translate(text, lang_tgt='en')  
-print(translate_text)
+# from google_trans_new import google_translator  
+# translator = google_translator()  
+# text= 'mặt hàng này rất tệ, không đúng với giá thành'
+# translate_text = translator.translate(text, lang_tgt='en')  
+# print(translate_text)
+# from django.db.models import Q
+# from ecomapp.models import Product
+
+# a =  Product.objects.all()
+# print(a)
